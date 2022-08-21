ed40163d019e4189ce215069b6bc7c8d4be4f57c
Renato Santana Brito <britotecst@outlook.com>
2022-08-21 16:39:10 -0300

    API Rest para Lesson
    Implementado Rest para Lesson com Validações

---
+++ src/main/java/com/ead/course/controller/LessonController.java
@@ -0,0 +1,94 @@
+package com.ead.course.controller;
+
+import com.ead.course.dtos.LessonDto;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.services.LessonService;
+import com.ead.course.services.ModuleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class LessonController {
+
+    LessonService lessonService;
+
+    ModuleService moduleService;
+
+    public LessonController(LessonService lessonService, ModuleService moduleService) {
+        this.lessonService = lessonService;
+        this.moduleService = moduleService;
+    }
+
+    @PostMapping("/modules/{moduleId}/lessons")
+    public ResponseEntity<Object> saveLesson(@PathVariable(value = "moduleId") UUID moduleId,
+                                             @RequestBody @Valid LessonDto lessonDto) {
+        Optional<ModuleModel> moduleModelOptional = moduleService.findById(moduleId);
+        if (!moduleModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found");
+        }
+        var lessonModule = new LessonModel();
+        BeanUtils.copyProperties(lessonDto, lessonModule);
+        lessonModule.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        lessonModule.setModule(moduleModelOptional.get());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(lessonService.save(lessonModule));
+    }
+
+    @DeleteMapping("/modules/{moduleId}/lessons/{lessonId}")
+    public ResponseEntity<Object> deleteLesson(@PathVariable(value = "moduleId") UUID moduleId,
+                                               @PathVariable(value = "lessonId") UUID lessonId) {
+
+        Optional<LessonModel> lessonModelOptional = lessonService.findLessonIntoModule(moduleId, lessonId);
+        if (lessonModelOptional.isPresent()) {
+            lessonService.delete(lessonModelOptional.get());
+            return ResponseEntity.status(HttpStatus.OK).body("Lesson deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Lesson not found for this module");
+        }
+    }
+
+    @PutMapping("/modules/{moduleId}/lessons/{lessonId}")
+    public ResponseEntity<Object> updateLesson(@PathVariable(value = "moduleId") UUID moduleId,
+                                               @PathVariable(value = "lessonId") UUID lessonId,
+                                               @RequestBody @Valid LessonDto lessonDto) {
+
+        Optional<LessonModel> lessonModelOptional = lessonService.findLessonIntoModule(moduleId, lessonId);
+        if (!lessonModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Lesson not found for this module");
+        }
+
+        var lessonModel = lessonModelOptional.get();
+        lessonModel.setTitle(lessonDto.getTitle());
+        lessonModel.setDescription(lessonDto.getDescription());
+        lessonModel.setVideoUrl(lessonDto.getVideoUrl());
+
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.save(lessonModel));
+    }
+
+    @GetMapping("/modules/{moduleId}/lessons")
+    public ResponseEntity<List<LessonModel>> getAllLessons(@PathVariable(value = "moduleId") UUID moduleId) {
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.findAllByModule(moduleId));
+    }
+
+    @GetMapping("/modules/{moduleId}/lessons/{lessonId}")
+    public ResponseEntity<Object> getOneLesson(@PathVariable(value = "moduleId") UUID moduleId,
+                                               @PathVariable(value = "lessonId") UUID lessonId) {
+        Optional<LessonModel> lessonModelOptional = lessonService.findLessonIntoModule(moduleId, lessonId);
+        if (lessonModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(lessonModelOptional.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Lesson not found for this module");
+        }
+    }
+}
--- src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -1,8 +1,13 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.services.LessonService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class LessonServiceImpl implements LessonService {
@@ -11,6 +16,26 @@ public class LessonServiceImpl implements LessonService {
 
     public LessonServiceImpl(LessonRepository lessonRepository) {
         this.lessonRepository = lessonRepository;
+    }
+
+    @Override
+    public LessonModel save(LessonModel lessonModule) {
+        return lessonRepository.save(lessonModule);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId) {
+        return lessonRepository.findLessonIntoModule(moduleId, lessonId);
+    }
+
+    @Override
+    public void delete(LessonModel lessonModel) {
+        lessonRepository.delete(lessonModel);
+    }
+
+    @Override
+    public List<LessonModel> findAllByModule(UUID moduleId) {
+        return lessonRepository.findAllLessonsIntoModule(moduleId);
     }
 }
 
